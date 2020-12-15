48d4bff9b2bd607ea4b446aa67ecef1d2d263361
Qki-H <75781808+Qki-H@users.noreply.github.com>
2020-12-15 22:56:29 +0100

    Add files via upload

---
+++ client_balance_2.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW client_balance_2 AS
+	SELECT i.client_id, c.name, CONCAT(address," ", city," ",state) AS full_address,
+			SUM(invoice_total-payment_total) AS balance
+	FROM invoices i
+	JOIN clients c USING(client_id)
+	WHERE (invoice_total-payment_total)>0
+    GROUP BY client_id, name
